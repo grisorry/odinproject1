aa472686af83f492a21a634c8ec221a611807d84
grisorry <graysintos@gmail.com>
2023-05-25 16:54:11 +0800

    completed assignment, testing

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,9 @@ router.get('/new', function(req, res, next) {
   res.render('form', {title: 'New Message'});
 })
 
-
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.message, user:req.body.name, added: new Date()});
+  res.redirect('/');
+});
 
 module.exports = router;
